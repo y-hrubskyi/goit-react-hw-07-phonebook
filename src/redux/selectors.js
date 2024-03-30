b860cc31a39fc79fce7ce01ff7e2ff599339c971
y-hrubskyi <y.hrubskyi@gmail.com>
2024-03-30 11:20:34 +0200

    chore: add more selectors

--- src/redux/selectors.js
+++ src/redux/selectors.js
@@ -1,7 +1,10 @@
 import { createSelector } from '@reduxjs/toolkit';
 
-export const selectFilter = state => state.filter;
 export const selectContacts = state => state.contacts.items;
+export const selectIsLoading = state => state.contacts.isLoading;
+export const selectError = state => state.contacts.error;
+
+export const selectFilter = state => state.filter;
 
 export const selectFilteredContacts = createSelector(
   [selectContacts, selectFilter],
